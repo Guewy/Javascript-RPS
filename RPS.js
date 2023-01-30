0105738671ac2189c3397b23cb0e5cda4b3e8d60
Guewy <119945451+Guewy@users.noreply.github.com>
2023-01-30 16:44:38 +0000

    Final version super streamlined, no validation

--- RPS.js
+++ RPS.js
@@ -1,84 +1,72 @@
 function getComputerChoice(){
-
-
-
+    
     //RNG for the computers choice
-    let x = Math.floor(Math.random()*3);
-    let choice = ""
-    
-
-    //Check the RNG choice to assign an outcome
-    if (x == 0){
-        choice = "rock"
-    }
-    else if (x == 1){
-        choice = "paper"
-    }
-    else {
-        choice = "scissors"
-    }
-    
-    console.log("The RNG computer choice is " + choice + ".");
-
-    return choice
+    RPS = ["rock", "paper", "scissors"];
+    return choice = RPS[Math.floor(Math.random()*3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    //Set an empty result variable
-    result = "";
     
     //Nested if statements to check all results
     if (playerSelection == computerSelection){
-        result = "tie";
-    }
-    else if (playerSelection == "rock"){
-        if (computerSelection == "scissors"){
+            result = "tie";
+        }
+    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "scissors" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "rock")){
             result = "player wins";
         }
-        else{
-            result = "computer wins";
-        }
+    else {
+        result = "computer wins";
     }
-    else if (playerSelection == "scissors"){
-        if (computerSelection == "paper"){
-            result = "player wins";
-        }
-        else{
-            result = "computer wins";
-        }
-    }
-    else if (playerSelection == "paper"){
-        if (computerSelection == "rock"){
-            result = "player wins";
-        }
-        else{
-            result = "computer wins";
-        }
-    }
-
-    //results check
-    console.log("The player choice is " + playerSelection + ".");
-    console.log("The computer choice is " + computerSelection + ".");
-    console.log("The result is " + result + ".");
     
     //Check and return certain results
     if (result == "player wins"){
-        return ("The player wins, " + playerSelection + " beats " +computerSelection + ".");
+        playerScore = playerScore + 1;
+        return ("The player wins, " + playerSelection + " beats " + computerSelection + ".");
     }
     else if (result == "computer wins"){
+        computerScore = computerScore + 1;
         return ("The computer wins, " + computerSelection + " beats " + playerSelection + ".");
     }
     else{
         return ("It's a tie! Two " + playerSelection + "s.");
     }
 }
-    //Get the computers choice
-    const playerSelection = "rock";
-    const computerSelection = getComputerChoice();
 
-    //Compare the two choices for the result and print to console
-    console.log(playRound(playerSelection, computerSelection));
+function game(){
 
+    //Ask the player for their choice
+    let playerSelection = prompt("Gimme your choice:").toLowerCase();
+ 
+    //Get the computer choice
+    let computerSelection = getComputerChoice();
 
+    //Compare the two choices for the result and return round result
 
+    //Advance the round
+    round = round + 1;
+
+    return ("Round " + round + ": " + playRound(playerSelection, computerSelection)); 
+}
+
+//Initialize the round number and score trackers
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+//Loop the game til it reaches the max round
+for(let i = 0; i<5; i++){
+    console.log(game());
+}
+
+//Declare the winner
+if(playerScore > computerScore){
+    console.log("You win! " + playerScore + "-" + computerScore +".");
+}
+else if(computerScore > playerScore){
+    console.log("You lose, big sad. " + playerScore + "-" + computerScore +".");
+}
+else{
+    console.log("It's a draw! " + playerScore + "-" + computerScore +".");
+}
