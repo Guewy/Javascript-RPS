dde29c7311dfbd6c9535993c736294d337e731c2
Guewy <119945451+Guewy@users.noreply.github.com>
2023-01-30 15:15:10 +0000

    First attempt

---
+++ RPS.js
@@ -0,0 +1,76 @@
+function getComputerChoice(){
+
+    //RNG for the computers choice
+    let x = Math.floor(Math.random()*3);
+    let choice = ""
+    
+    //Check the RNG choice to assign an outcome
+    if (x=0){
+        choice = "rock"
+    }
+    else if (x=1){
+        choice = "paper"
+    }
+    else {
+        choice = "scissors"
+    }
+    
+    return choice
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    //Set an empty result variable
+    result = "";
+    
+    //Nested if statements to check all results
+    if (playerSelection == computerSelection){
+        result = "tie";
+    }
+    if (playerSelection == "rock"){
+        if (computerSelection == "scissors"){
+            result = "player wins";
+        }
+        else{
+            result = "computer wins";
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "paper"){
+            result = "player wins";
+        }
+        else{
+            result = "computer wins";
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+            result = "player wins";
+        }
+        else{
+            result = "computer wins";
+        }
+    }
+    
+    //Check and return certain results
+    if (result = "player wins"){
+        return "The player wins, " + playerSelection + " beats " + computerSelection ".";
+    }
+    else if (result = "computer wins"){
+        return "The computer wins, " + computerSelection + " beats " + playerSelection ".";
+    }
+    else{
+        return "It's a tie! Two " + playerSelection + "s.";
+    }
+}
+    //Get the computers choice
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+
+
+
+    //Compare the two choices for the result and print to console
+    console.log(playRound(playerSelection, computerSelection));
+
+
+
